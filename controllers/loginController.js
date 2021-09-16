415345024e48576427cff510d7df8186a2ef0117
tulika03 <tulika@thelattice.in>
2021-09-16 15:10:16 +0530

    refresh token

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,8 +6,10 @@ let bcrypt = require('bcryptjs');
 let config = require("../config/app-config");
 let randToken = require('rand-token');
 let refreshTokens = {};
+
 userLogin = async function (req, res, next) {
     const errors = validationResult(req);
+    console.log(errors)
     if (!errors.isEmpty()) {
         let valError = errors.array().map(e => e.param + ' ');
         let errordata = valError.toString().slice(0, -1);
@@ -17,17 +19,20 @@ userLogin = async function (req, res, next) {
     let tempConnection;
     try {
         tempConnection = await mysql.connection();
-        let userData = await tempConnection.query("select user_id, user_type_id, user_name, user_password, salt, is_active, login_attempt from user_master where user_name = ?;", [req.body.username]);
-        await tempConnection.releaseConnection();
+        let userData = await tempConnection.query("select user_id, user_type_id, email, password, salt from user_master where email = ?;", [req.body.username]);
+      //  await tempConnection.releaseConnection();
+        console.log(userData)
         if (userData.length == 0) {
            return response.sendUnauthorizedError(res, 401, "Invalid username or password");
         }
-        else if (bcrypt.hashSync(req.body.password, userData[0].salt) == userData[0].user_password) {
+        else if (bcrypt.hashSync(req.body.password, userData[0].salt) == userData[0].password) {
             let userLogin = {};
             userLogin.user_id = userData[0].user_id;
             userLogin.user_type_id = userData[0].user_type_id;
             let refreshToken = randToken.uid(256);
             refreshTokens[refreshToken] = req.body.username;
+            await tempConnection.query("update user_master set refresh_token = ? where email = ?;", [refreshToken, req.body.username]);
+            await tempConnection.releaseConnection();
             userLogin.token = createToken({ user_id: userData[0].user_id, user_type_id: userData[0].user_type_id });
             userLogin.refreshToken = refreshToken;
            return response.sendSuccess(res, "Logged in successfully", userLogin)
@@ -45,11 +50,47 @@ userLogin = async function (req, res, next) {
 }
 
 
+userToken = async function (req, res, next) {
+    const errors = validationResult(req);
+    console.log(errors)
+    if (!errors.isEmpty()) {
+        let valError = errors.array().map(e => e.param + ' ');
+        let errordata = valError.toString().slice(0, -1);
+        return response.sendInvalidDataError(res, valError);
+    }
+
+    let tempConnection;
+    try {
+        tempConnection = await mysql.connection();
+        let userData = await tempConnection.query("select user_id, user_type_id, email, refresh_token from user_master where email = ?;", [req.body.username]);
+        if((userData[0].refresh_token == req.body.token)) {
+            let userLogin = {};
+            userLogin.user_id = userData[0].user_id;
+            userLogin.user_type_id = userData[0].user_type_id;
+            let refreshToken = randToken.uid(256);
+            refreshTokens[refreshToken] = req.body.username;
+            await tempConnection.query("update user_master set refresh_token = ? where email = ?;", [refreshToken, req.body.username]);
+            await tempConnection.releaseConnection();
+            userLogin.token = createToken({ user_id: userData[0].user_id, user_type_id: userData[0].user_type_id });
+            userLogin.refreshToken = refreshToken;
+            return response.sendSuccess(res, "token generated successfully", userLogin)
+        }
+        
+
+    }
+    catch (e) {
+        console.log(e);
+        await tempConnection.releaseConnection();
+        return response.sendError(res, e.code);
+    }
+}
+
 // create json wen token
 function createToken(user) {
     return jwt.sign(user, config.secretKey, { expiresIn: '120000ms' });
 }
 
 module.exports = {
-    userLogin: userLogin
+    userLogin: userLogin,
+    userToken: userToken
 }
--- router/api/loginRouter.js
+++ router/api/loginRouter.js
@@ -19,7 +19,27 @@ let loginController = require("../../controllers/loginController");
  * @returns {Error} 504 - Database connection error
  */
 
-router.post('/login', [check('username').notEmpty(), check('password').notEmpty().isLength({ min: 10, max: 50 })], loginController.userLogin);
+router.post('/login', [check('username').notEmpty(), check('password').notEmpty().isLength({ min: 5, max: 50 })], loginController.userLogin);
+
+
+/**
+ * @typedef RefreshToken
+ * @property {string} username.required - username - eg: rajendra@thelattice.in
+ * @property {string} token.required - refresh token - eg: tIN21u6Il2rqxphNhSjhrdQaTvaVrsCZRxBDc36jJFz3Xjth16KwAMiZsq69ZufAqPAjUa0YH3J4LHdWUvb3THRDCGY2kk5NXDxMmHjXYT8cyK7Dbc60eeYGUzPlO26Fd8x71YikUuHS54N9bFCvxzAe5DKGEMJ5l5jd7M5NlvRp3MKhIdG2PBGQDsp4HftavIS9IyaGYtuLQfatxpg8RRgV4Gaqbq1e3vZov3rNsFjhu3MSaRb3WNEhG9wRt8Dw
+ */
+
+/**
+ * @route post /newToken
+ * @group Authorization
+ * @param {RefreshToken.model} refresh_token.body.required
+ * @returns {object} 200 - Logged in successfully
+ * @returns {Error} 400 - Invalid Credentials
+ * @returns {Error} 422 - Invalid or missing parameter
+ * @returns {Error} 500 - Unable to process
+ * @returns {Error} 504 - Database connection error
+ */
+
+ router.post('/newToken', [check('username').notEmpty(), check('token').notEmpty().isLength({ min: 5, max: 300 })], loginController.userToken);
 
 
 module.exports = router;
